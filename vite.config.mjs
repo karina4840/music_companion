61358f85fc91b303c372ac978099a7ccdf1c8c63
karina ivanova <86211129+karina4840@users.noreply.github.com>
2025-06-16 14:01:43 +0100

    deployment upd

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,8 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: '/music_companion/',
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
