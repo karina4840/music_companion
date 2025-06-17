0b966d2282d591cced80a1362891f850ca53f9fc
karina ivanova <86211129+karina4840@users.noreply.github.com>
2025-06-17 18:31:24 +0100

    Fix Netlify deployment configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/music_companion/',
+  base: '/',
   build: {
-    outDir: "build",
+    outDir: "dist",
     chunkSizeWarningLimit: 2000,
   },
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), netlify()],
   server: {
     port: "4028",
     host: "0.0.0.0",
